bbc217f3d5bd9ace7a256925aa5fcdd739bd7051
Shoaib Hamza <120443358+F2022065283@users.noreply.github.com>
2022-12-17 18:38:29 +0500

    Add files via upload

---
+++ aaaaaa.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    cout<<"Enter your marks";
+    cin>>a;
+    if(a>=33)
+    cout<<"Pass";
+    else
+    cout<<"Fail";
+    return 0;
+}
